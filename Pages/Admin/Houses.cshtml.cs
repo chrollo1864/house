a58468a0a834a31c07b3fcbe4a5a63b412652536
chrollo1864 <cpe.bisco.charles@gmail.com>
2025-05-02 06:35:58 +0800

    admin house edit

--- Pages/Admin/Houses.cshtml.cs
+++ Pages/Admin/Houses.cshtml.cs
@@ -19,6 +19,8 @@ namespace HouseApp.Pages.Admin
         }
 
         public IList<House> Houses { get; set; }
+        public IList<Location> Locations { get; set; }
+        public IList<PropertyType> PropertyTypes { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -26,6 +28,59 @@ namespace HouseApp.Pages.Admin
                 .Include(h => h.PropertyType)
                 .Include(h => h.Location)
                 .ToListAsync();
+
+            Locations = await _context.Locations.ToListAsync();
+            PropertyTypes = await _context.PropertyTypes.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string title, string address, decimal price, int locationId, int propertyTypeId, bool isAvailable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var house = new House
+            {
+                Title = title,
+                Address = address,
+                Price = price,
+                LocationId = locationId,
+                PropertyTypeId = propertyTypeId,
+                IsAvailable = isAvailable,
+                RegisteredDate = System.DateTime.Now
+            };
+
+            _context.Houses.Add(house);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostEditAsync(int id, string title, string address, decimal price, int locationId, int propertyTypeId, bool isAvailable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var house = await _context.Houses.FindAsync(id);
+
+            if (house == null)
+            {
+                return NotFound();
+            }
+
+            house.Title = title;
+            house.Address = address;
+            house.Price = price;
+            house.LocationId = locationId;
+            house.PropertyTypeId = propertyTypeId;
+            house.IsAvailable = isAvailable;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
         }
     }
 }
